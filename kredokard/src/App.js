39bce062e2e8c9a0a1d1a8f4e7481be9df2db55f
LeonitKelmendi <leonitkelmendi7@gmail.com>
2025-04-25 20:18:56 +0200

    Tarifnik done

--- kredokard/src/App.js
+++ kredokard/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import LoanApplication from './pages/LoanApplication';
-import MainContent from './pages/MainContent';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoanApplication from "./pages/LoanApplication";
+import MainContent from "./pages/MainContent";
+import Tarifnik from "./pages/Tarifnik";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoanApplication />} />
           <Route path="/kako-da-platam" element={<MainContent />} />
+          <Route path="/tarifnik" element={<Tarifnik />} />
         </Routes>
         <Footer />
       </div>
